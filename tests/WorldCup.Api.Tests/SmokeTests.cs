9827219e9dd9415438bcaa0d9d6ef35eb33ebed9
Espen Ekvang <espen.ekvang@moller.no>
2026-04-11 21:16:48 +0200

    chore(test): set up xUnit test project for backend

---
+++ tests/WorldCup.Api.Tests/SmokeTests.cs
@@ -0,0 +1,10 @@
+namespace WorldCup.Api.Tests;
+
+public class SmokeTests
+{
+    [Fact]
+    public void TestProjectBuildsAndRuns()
+    {
+        Assert.True(true);
+    }
+}
---
+++ tests/WorldCup.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WorldCup.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
