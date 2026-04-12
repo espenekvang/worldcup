41b61568be3a04029d505263567f2444066d7bfa
Espen Ekvang <espen.ekvang@moller.no>
2026-04-12 08:37:20 +0200

    fix: delete erroneous round-of-32 predictions via data migration

---
+++ api/WorldCup.Api/Migrations/20260412063648_DeleteRoundOf32Predictions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorldCup.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteRoundOf32Predictions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "DELETE FROM Predictions WHERE MatchId IN (73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
